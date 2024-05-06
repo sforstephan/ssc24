3355fc80a04e43f5ce0653f16cdb6efea6d8e8ff
Stephan <stephan.leitner@icloud.com>
2024-05-06 13:05:58 +0200

    Create variance_stability.py

---
+++ analysis_scripts/variance_stability.py
@@ -0,0 +1,28 @@
+import json
+import pandas as pd
+import os
+
+
+def main():
+
+    file_path = os.path.join('datasets', 'data_variance_stability.csv')
+    df = pd.read_csv(file_path)
+
+    # fix conditions following the coding explained in the main file
+    condition1 = df["matrix"] == 1
+    condition2 = df["coordination"] == 4
+    condition3 = df["repetitions"] == 1000
+
+    fitness_cols = [str(i) for i in range(0, 500)]
+    df_analysis = df.loc[df[condition1 & condition2 & condition3].index, fitness_cols]
+
+    # Compute the standard deviation, the mean and the Coefficient of Variation
+    sigma = df_analysis.values.flatten().std()
+    mu = df_analysis.values.flatten().mean()
+    # Print the result
+    print(f"Coefficient of Variation for selected scenario: {round(sigma / mu, 3)}")
+
+
+
+if __name__ == "__main__":
+    main()
